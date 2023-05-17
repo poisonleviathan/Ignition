df0959237aa55f4eeafb962e6d6d91199e183865
dinilgamage <113094888+dinilgamage@users.noreply.github.com>
2023-05-17 10:34:17 +0530

    init_member

--- Ignition/Program.cs
+++ Ignition/Program.cs
@@ -16,7 +16,7 @@ namespace Ignition
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new New_Member());
+            Application.Run(new Form1());
         }
     }
 }
