d1ee803aa40edc618ba480e1e3df69ba53094e3e
evinfer <113667938+evinfer@users.noreply.github.com>
2023-06-01 19:44:49 -0700

    Update Program.cs
    change proceed to Admin_Dashboard

--- Ignition/Program.cs
+++ Ignition/Program.cs
@@ -18,7 +18,7 @@ namespace Ignition
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new proceed());
+            Application.Run(new Admin_Dashboard());
         }
     }
 }
